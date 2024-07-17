79ecf8c658e7b1a876eb735d719b61078472c89b
yangpeng <1340983289@qq.com>
2024-07-17 17:27:59 +0800

    构建项目首次提交

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <Header />
+  <router-view></router-view>
+</div>
+  
+</template>
+
+<script lang="ts" setup>
+  import Header from '@/views/Header.vue'
+
+</script>
+
+<style scoped>
+
+</style>
