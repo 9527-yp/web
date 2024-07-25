9d45980cb4a6e3d6ad8e4befdc911e4d78e3f39c
yangpeng <1340983289@qq.com>
2024-07-25 10:06:27 +0800

    fix:修改菜单数据重复显示问题

--- src/layout/components/Menu.vue
+++ src/layout/components/Menu.vue
@@ -27,108 +27,14 @@ const activeMenu = computed(() => (route.meta.activeMenu ? route.meta.activeMenu
 const menuAnimate = ref('animate__animated animate__fadeInLeft')
 </script>
 
-
 <style lang="less" scoped>
 .el-menu{
     border-right:none;
 }
-/** el-menu鼠标悬停和点击代码 */
-// 父节点
-.el-menu-item {
-  height: 40px !important;
-  margin-bottom: 3px;
-  font-weight: 500;
-  // color: #000000;
-
-  /* 设置用户禁止选中 */
-  user-select: none;
-  border-left: 6px solid transparent;
-  border-radius: 6px;
-
-  // @apply dark:c-#E5E3FA;
-  color: var(--el-menu-text-color);
-  i {
-    transform: translate(-8px); // 图标偏移
-  }
-  span {
-    transform: translate(-8px); // 文字偏移
-  }
-
-  // 设置鼠标悬停时el-menu-item的样式
-  &:hover {
-    // color: var(--el-color-primary);
-    color: #fa7e23;
-    // background: var(--el-color-primary-light-8);
-    background: #fee5d3;
-    border-left: 6px solid #fa7e23;
-
-    // & 含义 .el-menu-item
-    border-radius: 6px;
-
-    // 实现鼠标悬停时icon变色
-    i {
-      // color: var(--el-color-primary);
-      color: #fa7e23;
-    }
-  }
-
-  // 设置选中el-menu-item时的样式
-  &.is-active {
-    // color: var(--el-color-primary);
-    color: #fa7e23;
-    // background: var(--el-color-primary-light-8);
-    background: #fee5d3;
-    // border-left: $aside-menu-border-left solid var(--el-color-primary);
-    border-left: 6px solid #fa7e23;
-  }
-}
-:deep(.el-sub-menu__title) {
-  height: 40px;
-  padding-right: 0; // 去除collapse缩小多余的边框
-  margin-bottom: 3px;
-  font-weight: 500;
-
-  /* 设置用户禁止选中 */
-  user-select: none;
-  border-left: 6px solid transparent;
-  border-radius: 6px;
-
-  // @apply dark:c-#E5E3FA;
-//   color: #fa7e23;
-  i {
-    transform: translate(-8px); // 图标偏移
-  }
-  span {
-    transform: translate(-8px); // 文字偏移
-  }
-  &:hover {
-    // color: var(--el-color-primary);
-    color: #fa7e23;
-    // background: var(--el-color-primary-light-8);
-    background: #fee5d3;
-    // border-left: $aside-menu-border-left solid var(--el-color-primary);
-    border-left: 6px solid #fa7e23;
-  }
-  &:active {
-    // color: var(--el-color-primary);
-    color: #fa7e23;
-    // background: var(--el-color-primary-light-8);
-    background: #fee5d3;
-    // border-left: $aside-menu-border-left solid var(--el-color-primary);
-    border-left: 6px solid #fa7e23;
-  }
-}
-
 </style>
 <style lang="less">
-.el-sub-menu.is-active > .el-sub-menu__title {
-  // color: var(--el-color-primary) !important;
-  color: #fa7e23 !important;
-}
-
-/* icon图标也跟着变 */
-.el-sub-menu.is-active > .el-sub-menu__title i {
-  // color: var(--el-color-primary) !important;
-  color: #fa7e23 !important;
+/** 菜单悬浮折叠宽度 */
+.el-menu--collapse {
+  width: 70px !important;
 }
 </style>
--- src/layout/components/MenuList.vue
+++ src/layout/components/MenuList.vue
@@ -10,7 +10,7 @@
             <MenuList :menuList="item.children" />
         </el-sub-menu>
         <!-- 叶子节点[功能节点] -->
-        <el-menu-item>
+        <el-menu-item v-else :index="item.path">
             <el-icon><component :is="item.icon"></component></el-icon>
             <span>{{ item.meta.title }}</span>
         </el-menu-item>
@@ -23,3 +23,108 @@
 
     import MenuList from '@/layout/components/MenuList.vue'
 </script>
+
+<style lang="less" scoped>
+.el-menu{
+    border-right:none;
+}
+/** el-menu鼠标悬停和点击代码 */
+// 父节点
+.el-menu-item {
+  height: 40px !important;
+  margin-bottom: 3px;
+  font-weight: 500;
+  // color: #000000;
+
+  /* 设置用户禁止选中 */
+  user-select: none;
+  border-left: 6px solid transparent;
+  border-radius: 6px;
+
+  // @apply dark:c-#E5E3FA;
+  color: var(--el-menu-text-color);
+  i {
+    transform: translate(-8px); // 图标偏移
+  }
+  span {
+    transform: translate(-8px); // 文字偏移
+  }
+
+  // 设置鼠标悬停时el-menu-item的样式
+  &:hover {
+    // color: var(--el-color-primary);
+    color: #fa7e23;
+    // background: var(--el-color-primary-light-8);
+    background: #fee5d3;
+    border-left: 6px solid #fa7e23;
+
+    // & 含义 .el-menu-item
+    border-radius: 6px;
+
+    // 实现鼠标悬停时icon变色
+    i {
+      // color: var(--el-color-primary);
+      color: #fa7e23;
+    }
+  }
+
+  // 设置选中el-menu-item时的样式
+  &.is-active {
+    // color: var(--el-color-primary);
+    color: #fa7e23;
+    // background: var(--el-color-primary-light-8);
+    background: #fee5d3;
+    // border-left: $aside-menu-border-left solid var(--el-color-primary);
+    border-left: 6px solid #fa7e23;
+  }
+}
+:deep(.el-sub-menu__title) {
+  height: 40px;
+  padding-right: 0; // 去除collapse缩小多余的边框
+  margin-bottom: 3px;
+  font-weight: 500;
+
+  /* 设置用户禁止选中 */
+  user-select: none;
+  border-left: 6px solid transparent;
+  border-radius: 6px;
+
+  // @apply dark:c-#E5E3FA;
+//   color: #fa7e23;
+  i {
+    transform: translate(-8px); // 图标偏移
+  }
+  span {
+    transform: translate(-8px); // 文字偏移
+  }
+  &:hover {
+    // color: var(--el-color-primary);
+    color: #fa7e23;
+    // background: var(--el-color-primary-light-8);
+    background: #fee5d3;
+    // border-left: $aside-menu-border-left solid var(--el-color-primary);
+    border-left: 6px solid #fa7e23;
+  }
+  &:active {
+    // color: var(--el-color-primary);
+    color: #fa7e23;
+    // background: var(--el-color-primary-light-8);
+    background: #fee5d3;
+    // border-left: $aside-menu-border-left solid var(--el-color-primary);
+    border-left: 6px solid #fa7e23;
+  }
+}
+
+</style>
+<style lang="less">
+.el-sub-menu.is-active > .el-sub-menu__title {
+  // color: var(--el-color-primary) !important;
+  color: #fa7e23 !important;
+}
+
+/* icon图标也跟着变 */
+.el-sub-menu.is-active > .el-sub-menu__title i {
+  // color: var(--el-color-primary) !important;
+  color: #fa7e23 !important;
+}
+</style>
